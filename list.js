4178324009de5e27e0ec0cc054bfe263c492b1ff
DanielleSaint <daniellesaint09@gmail.com>
2025-04-24 12:57:17 -0500

    Tarea finalizada

--- list.js
+++ list.js
@@ -1,17 +1,27 @@
-const formulario = document.getElementById('form-tarea');
+const botonAgregar = document.getElementById('botonTarea')
 
-formulario.addEventListener('submit', function (e) {
+botonAgregar.addEventListener('click', function (e) {
     e.preventDefault();
     //Traigo el valor de la tarea al agregar
-    const titulo = document.getElementById('nombreTarea').value;
-    const descripcionTarea = document.getElementById('descripcion').value;
-    const categoria = document.getElementById('categoriaTarea').value;
-    //Creo una nueva tarea dentro de una categoria
-    const nuevaTarea = document.createElement('div');
-    nuevaTarea.classList.add('tarea', categoria);
-    nuevaTarea.innerHTML = `
-    <h3>${titulo}</h3>
-    <p>${descripcionTarea}</p>
+    const titulo = document.getElementById('nombreTarea').value.trim();
+    const descripcionTarea = document.getElementById('descripcion').value.trim();
+    if (!titulo || !descripcionTarea) {
+        alert("Por favor, completa todos los campos.");
+        return;
+    }
+    //Creo una nueva lista
+    const nuevaLista = document.createElement('li');
+    nuevaLista.className = 'listaToDo';
+    nuevaLista.innerHTML = `
+        <div>
+            <img src="./img/pngtree-purple-round-border-image_2231965-removebg-preview.png">
+            <div>
+                <h2>${titulo}</h2>
+                <p>${descripcionTarea}</p>
+            </div>
+            <button type=submit id="eliminar">Eliminar</button>
+        </div>
     `;
-
-})
+    //agregar nueva lista al listado
+    document.getElementById("listado").appendChild(nuevaLista);
+});
