41f9f7550a0bfcce1ec5d049c5c416c8913d1b2d
DanielleSaint <daniellesaint09@gmail.com>
2025-04-24 13:04:28 -0500

    Tarea completada

--- list.js
+++ list.js
@@ -14,7 +14,7 @@ botonAgregar.addEventListener('click', function (e) {
     nuevaLista.className = 'listaToDo';
     nuevaLista.innerHTML = `
         <div>
-            <img src="./img/pngtree-purple-round-border-image_2231965-removebg-preview.png">
+            <img src="./img/pngtree-purple-round-border-image_2231965-removebg-preview.png" class="tareaCompletada">
             <div>
                 <h2>${titulo}</h2>
                 <p>${descripcionTarea}</p>
@@ -24,4 +24,19 @@ botonAgregar.addEventListener('click', function (e) {
     `;
     //agregar nueva lista al listado
     document.getElementById("listado").appendChild(nuevaLista);
+    //limpiar cuadros de tareas
+    document.getElementById('nombreTarea').value = '';
+    document.getElementById('descripcion').value = '';
+
+    // Cambiar imagen completada
+    const img = nuevaLista.querySelector('.tareaCompletada');
+    img.addEventListener('click', function () {
+        img.src = './img/png-transparent-check-mark-symbol-purple-steven-universe-amethyst-gemstone-logo-violet-circle-removebg-preview.png';
+    });
+
+    // Eliminar tarea
+    const botonEliminar = nuevaLista.querySelector('#eliminar');
+    botonEliminar.addEventListener('click', function () {
+        nuevaLista.remove();
+    });
 });
